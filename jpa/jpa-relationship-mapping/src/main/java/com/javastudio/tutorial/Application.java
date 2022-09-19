dafed8af9f2d6b457b5e97a9c9a4ce67ebb28228
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-09-19 15:01:58 +0200

    jpa relationship mapping

--- jpa/jpa-relationship-mapping/src/main/java/com/javastudio/tutorial/Application.java
+++ jpa/jpa-relationship-mapping/src/main/java/com/javastudio/tutorial/Application.java
@@ -1,12 +1,15 @@
 package com.javastudio.tutorial;
 
+import com.javastudio.tutorial.model.Product;
+import com.javastudio.tutorial.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Application {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        LOGGER.info("Application started");
+        LOGGER.info("Application started!");
     }
 }
